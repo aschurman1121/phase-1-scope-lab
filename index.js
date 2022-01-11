aab9e2e19a53551923592faff52dacd9c6a3ee09
alexandra <alexandraschurman@gmail.com>
2022-01-11 15:35:26 -0800

    commit tree 0a5622f3707633d10e1ac2368d2de922c9c0b165

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'name'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'different name'
+    return changeLeastFavoriteCustomer
+}
+
+/*
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+// customerName = upperCaseCustomerName();
+
+
+// console.log(upperCaseCustomerName())
+
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = ('not bob')
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"   
+}
+
+const leastFavoriteCustomer = "sue";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'non'
+}
+
+// console.log(setBestCustomer);
+
+// function overwriteBestCustomer(){
+//     bestCustomer.toUpperCase();
+// }
+
+*/
